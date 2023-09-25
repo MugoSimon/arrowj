731133c04edcb940e91482933b1c82d4aacae327
Mugo Simon <mugolastbon@gmail.com>
2023-09-25 20:37:53 +0300

    home, updated home screen

--- lib/features/authentication/screens/welcome/welcome.dart
+++ lib/features/authentication/screens/welcome/welcome.dart
@@ -1,29 +1,69 @@
+import 'package:arrowj/constants/colors.dart';
 import 'package:arrowj/constants/image_strings.dart';
+import 'package:arrowj/constants/sizes.dart';
 import 'package:arrowj/constants/text_strings.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.all(tDefaultSize),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Image(image: AssetImage(tWelcomeScreenImage)),
-            Text(tWelcomeTitle),
-            Text(tWelcomeSubTitle),
+            Image(
+              image: AssetImage(tWelcomeScreenImage),
+              height: height * 0.6,
+            ),
+            Column(
+              children: [
+                Text(tWelcomeTitle,
+                    style: Theme.of(context).textTheme.headlineMedium),
+                Text(
+                  tWelcomeSubTitle,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
             Row(
               children: [
-                OutlinedButton(
-                  onPressed: () {},
-                  child: Text(tLogin,
-                      style: Theme.of(context).textTheme.bodyLarge),
-                ),
-                ElevatedButton(
+                Expanded(
+                  child: OutlinedButton(
                     onPressed: () {},
-                    child: Text(tSignUp,
-                        style: Theme.of(context).textTheme.bodyLarge))
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(),
+                      foregroundColor: tSecondaryColor,
+                      side: BorderSide(color: tSecondaryColor),
+                      padding: EdgeInsets.symmetric(vertical: tButtonHeight),
+                    ),
+                    child: Text(tLogin.toUpperCase(),
+                        style: Theme.of(context).textTheme.bodyLarge),
+                  ),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(),
+                      foregroundColor: tWhiteColor,
+                      backgroundColor: tSecondaryColor,
+                      side: BorderSide(color: tWhiteColor),
+                      padding: EdgeInsets.symmetric(vertical: tButtonHeight),
+                      textStyle: TextStyle(color: Colors.white),
+                    ),
+                    child: Text(
+                      tSignUp.toUpperCase(),
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                  ),
+                )
               ],
             )
           ],
