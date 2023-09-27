dfa2e517bb1bdaebaf6eca49bcf51965971607dc
Mugo Simon <mugolastbon@gmail.com>
2023-09-27 08:38:39 +0300

    starter working correctly

--- lib/features/others/fade_in_animation/fade_in_animation_controller.dart
+++ lib/features/others/fade_in_animation/fade_in_animation_controller.dart
@@ -1,6 +1,6 @@
-import 'package:arrowj/constants/image_strings.dart';
+
+import 'package:arrowj/features/authentication/screens/welcome/welcome.dart';
 import 'package:get/get.dart';
-import 'package:flutter/material.dart';
 import 'package:arrowj/features/authentication/screens/on_boarding_screen/on_boarding_screen.dart';
 
 class FadeInAnimationController extends GetxController {
@@ -14,7 +14,7 @@ class FadeInAnimationController extends GetxController {
     await Future.delayed(Duration(milliseconds: 3000));
     animate.value = false;
     await Future.delayed(Duration(milliseconds: 2000));
-    Get.offAll(() => OnBoardingScreen());
+    Get.offAll(() => WelcomeScreen());
   }
 
   Future AnimationOut() async {
